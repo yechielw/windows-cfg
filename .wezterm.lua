585610fd6e5d4df2eca3ac23aab9e6536cd78268
Yechiel Worenklein <yechiel.worenklein@cybersafe.co.il>
2024-04-05 20:46:05 +0300

    fome fizes and added espanto

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,24 +1,13 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
--- This table will hold the configuration.
-local config = {}
+local config = wezterm.config_builder()
 
-
-
-
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
-
-
-
-
-config.color_scheme = 'Catppuccin Mocha'
-config.font = wezterm.font 'JetBrains Mono'
+config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font("JetBrains Mono")
 config.font_size = 14.0
-config.window_background_opacity = 0.9
-config.default_prog = {'wsl','tmux','a'}
+config.window_background_opacity = 0.97
+config.default_prog = { "wsl", "tmux", "a" }
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 
